3b1e8222f8b66d338f84639e01859a95cef2cab4
gibmalfeuerzeug <janisbro031@gmail.com>
2025-11-26 21:22:14 +0100

    Update and rename app.py to main.py

--- main.py
+++ main.py
@@ -46,4 +46,4 @@ async def update_prefix(request: Request, guild_id: int, prefix: str):
     )
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
